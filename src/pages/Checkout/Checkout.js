4e58c0c6c4c4a33bdaf8720b3cbed366c8e76843
Bernardo <os.bernardo@outlook.com>
2021-12-07 18:23:33 -0600

    Add checkout buttons

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,6 +1,7 @@
 import CustomerForm from "components/CustomerForm/CustomerForm";
 import OrderSummary from "components/OrderSummary/OrderSummary";
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { SectionContainer } from "StyledComponents";
 
@@ -9,12 +10,30 @@ const ContentContainer = styled.div`
   text-align: left;
 `;
 
+const CheckoutButton = styled.button`
+  padding: 15px;
+  border-radius: 8px;
+  cursor: pointer;
+  margin: 15px;
+  font-size: 1.3em;
+  border: black;
+`;
+
+const PlaceOrderButton = styled(CheckoutButton)`
+  background-color: black;
+  color: white;
+`;
+
 function Checkout() {
   return (
     <ContentContainer>
       <SectionContainer>
         <CustomerForm />
         <OrderSummary />
+        <Link to="/cart">
+          <CheckoutButton>Go back to cart</CheckoutButton>
+        </Link>
+        <PlaceOrderButton>Place order</PlaceOrderButton>
       </SectionContainer>
     </ContentContainer>
   );
