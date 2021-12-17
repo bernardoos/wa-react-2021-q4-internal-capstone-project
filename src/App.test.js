96da58adba882d8bfadf4d3827b206d62534397f
Bernardo <os.bernardo@outlook.com>
2021-12-17 00:21:24 -0600

    PR comments

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders app logo", async () => {
+test("renders app logo", () => {
   render(<App />);
   const linkElement = screen.getByAltText("Logo");
   expect(linkElement).toBeInTheDocument();
